25581a73e678b2b14a2fe3c28e827259d590327b
roksys <rokas.ma@gmail.com>
2017-06-26 13:23:50 +0200

    Changed connectivity test

--- PSST/tests/check_connection.sh
+++ PSST/tests/check_connection.sh
@@ -2,17 +2,19 @@
 #
 # Script to test connectivity
 
-# -q: Silence mode; --sipder: don't get, just check page availability
-wget -q --spider http://cern.ch
+# -z just scan for listening daemons, without sending any data to them.
+# -v more verbose output
+# -w timeout
+/usr/bin/nc -zv -w 5 dashb-mb.cern.ch 61113
 if [ $? -eq 0 ]; then
-	echo "Online"
-	exit 0
+  echo "Online"
+  exit 0
 fi
 
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	echo "Online"
+/usr/bin/nc -zv -w 5 google.com 80
+if [ $? -ne 0 ]; then
+  echo "Online"
 else
-	echo $ERROR_NO_CONNECTION_MSG
-	exit $ERROR_NO_CONNECTION
+  echo $ERROR_NO_CONNECTION_MSG
+  exit $ERROR_NO_CONNECTION
 fi
